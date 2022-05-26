b68c9ae6348bc49e92a322e7b9eef2650bffcc2b
Jürgen Efeish <jefeish@github.com>
2022-05-26 19:17:49 -0400

    init

--- src/index.js
+++ src/index.js
@@ -11,14 +11,18 @@ const github = require('@actions/github');
 
     try {
         const prefixes = core.getInput("prefixes")
+        const refBranch = core.getInput("refBranch")
         const token = core.getInput("GITHUB_TOKEN")
         const octokit = github.getOctokit(token)
         const context = github.context
         const owner = github.context.repo.owner
         const repo = github.context.repo.repo
 
-        console.log(' owner: ' + owner + '\n repo: ' + repo)
+        console.log('owner: ' + owner)
+        console.log('repo: ' + repo)
         console.log('GITHUB_TOKEN: ' + token)
+        console.log('prefixes: ' + prefixes)
+        console.log('refBranch: ' + refBranch)
         console.log(checkMergability(1, 2))
         const res = await getPullRequest(1, owner, repo, octokit)
         console.log(res)
